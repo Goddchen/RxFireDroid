979e6a9d6f104b8436e1724b160fa1f29e99bb08
Goddchen <goddchen@gmail.com>
2016-12-04 18:15:32 +0100

    add integration tests (untested)

---
+++ library/app/src/androidTest/java/RxFireDroidDatabaseTest.java
@@ -0,0 +1,19 @@
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by goddc on 04.12.2016.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RxFireDroidDatabaseTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(true, true);
+    }
+}
