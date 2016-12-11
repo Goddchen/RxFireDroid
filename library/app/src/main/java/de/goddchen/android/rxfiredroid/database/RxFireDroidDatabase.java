d3356d29b85741aa034c929681c11804d32a1a4d
Goddchen <goddchen@googlemail.com>
2016-12-11 17:57:31 +0100

    add support for child events

--- library/app/src/main/java/de/goddchen/android/rxfiredroid/database/RxFireDroidDatabase.java
+++ library/app/src/main/java/de/goddchen/android/rxfiredroid/database/RxFireDroidDatabase.java
@@ -1,5 +1,6 @@
 package de.goddchen.android.rxfiredroid.database;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -61,22 +62,7 @@ public class RxFireDroidDatabase {
     }
 
     public static Single<DataSnapshot> getValues(DatabaseReference ref) {
-        return Single.<DataSnapshot>create(emitter ->
-                ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if (!emitter.isDisposed()) {
-                            emitter.onSuccess(dataSnapshot);
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        emitter.onError(databaseError.toException());
-                    }
-                }))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+        return getValues((Query) ref);
     }
 
     public static Single<DataSnapshot> getValues(String pathFormat, Object... args) {
@@ -108,32 +94,7 @@ public class RxFireDroidDatabase {
     }
 
     public static Observable<DataSnapshot> observeValues(DatabaseReference ref, boolean receiveInitialValues) {
-        return Observable.<DataSnapshot>create(subscriber -> {
-            boolean[] initial = new boolean[]{true};
-            ValueEventListener valueEventListener = new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (initial[0]) {
-                        initial[0] = false;
-                        if (receiveInitialValues) {
-                            subscriber.onNext(dataSnapshot);
-                        }
-                    } else {
-                        subscriber.onNext(dataSnapshot);
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    subscriber.onError(databaseError.toException());
-                }
-            };
-            subscriber.setDisposable(Disposables.fromAction(() ->
-                    ref.removeEventListener(valueEventListener)));
-            ref.addValueEventListener(valueEventListener);
-        })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+        return observeValues((Query) ref, receiveInitialValues);
     }
 
     public static Observable<DataSnapshot> observeValues(Query query, boolean receiveInitialValues) {
@@ -209,6 +170,74 @@ public class RxFireDroidDatabase {
         return Observable.just(value)
                 .toMap(toKey -> ref, toValue -> value)
                 .flatMapCompletable(RxFireDroidDatabase::setValues);
+    }
+
+    public static Observable<ChildEvent> observeChildren(String pathFormat, Object... args) {
+        return observeChildren(String.format(pathFormat, args));
+    }
+
+    public static Observable<ChildEvent> observeChildren(String path) {
+        return getRef(path)
+                .flatMapObservable(RxFireDroidDatabase::observeChildren);
+    }
+
+    public static Observable<ChildEvent> observeChildren(DatabaseReference databaseReference) {
+        return observeChildren((Query) databaseReference);
+    }
+
+    public static Observable<ChildEvent> observeChildren(Query query) {
+        return Observable.<ChildEvent>create(emitter -> {
+            final ChildEventListener childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    emitter.onNext(new ChildEvent(ChildEvent.TYPE.ADDED, dataSnapshot, s));
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    emitter.onNext(new ChildEvent(ChildEvent.TYPE.CHANGED, dataSnapshot, s));
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    emitter.onNext(new ChildEvent(ChildEvent.TYPE.REMOVED, dataSnapshot, null));
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    emitter.onNext(new ChildEvent(ChildEvent.TYPE.MOVED, dataSnapshot, s));
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    emitter.onError(databaseError.toException());
+                }
+            };
+            emitter.setDisposable(Disposables.fromAction(() ->
+                    query.removeEventListener(childEventListener)));
+            query.addChildEventListener(childEventListener);
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static class ChildEvent {
+        public TYPE type;
+        public DataSnapshot dataSnapshot;
+        public String previousChildKey;
+
+        public ChildEvent(TYPE type, DataSnapshot dataSnapshot, String previousChildKey) {
+            this.type = type;
+            this.dataSnapshot = dataSnapshot;
+            this.previousChildKey = previousChildKey;
+        }
+
+        public enum TYPE {
+            ADDED,
+            CHANGED,
+            REMOVED,
+            MOVED
+        }
     }
 
 }
