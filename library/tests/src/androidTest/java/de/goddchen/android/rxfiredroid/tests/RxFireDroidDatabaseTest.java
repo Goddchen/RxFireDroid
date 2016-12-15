d29bdfac3f129787ed66dc2ee7a0ea14fe825a2d
Goddchen <goddchen@googlemail.com>
2016-12-15 01:36:53 +0100

    add a test for path format arguments

--- library/tests/src/androidTest/java/de/goddchen/android/rxfiredroid/tests/RxFireDroidDatabaseTest.java
+++ library/tests/src/androidTest/java/de/goddchen/android/rxfiredroid/tests/RxFireDroidDatabaseTest.java
@@ -2,12 +2,15 @@ package de.goddchen.android.rxfiredroid.tests;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.firebase.database.DatabaseReference;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import de.goddchen.android.rxfiredroid.database.RxFireDroidDatabase;
+import io.reactivex.functions.Function;
 
 /**
  * Created by goddc on 04.12.2016.
@@ -30,5 +33,18 @@ public class RxFireDroidDatabaseTest {
                 .blockingAwait();
         RxFireDroidDatabase.deleteValues("test-value").blockingAwait();
         Assert.assertFalse(RxFireDroidDatabase.getValues("test-value").blockingGet().exists());
+    }
+
+    @Test
+    public void pathFormatTest() {
+        RxFireDroidDatabase.getRef("test/%s", "arg")
+                .map(new Function<DatabaseReference, String>() {
+                    @Override
+                    public String apply(DatabaseReference databaseReference) throws Exception {
+                        return databaseReference.getKey();
+                    }
+                })
+                .test()
+                .assertValue("arg");
     }
 }
